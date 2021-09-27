0ea89fc79c290ffd27909790625dd3c4b5fccc1e
dylanmeuriot <dylanmeuriot@gmail.com>
2021-09-27 14:25:10 +0200

    Commit 1 : Modules

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { discardPeriodicTasks } from '@angular/core/testing';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class LoginRoutingModule { }
