8e0cf44988dd6651c9a558a3d7212671094d20d0
dylanmeuriot <dylanmeuriot@gmail.com>
2021-09-30 10:41:57 +0200

    Commit 24 : Exercice “version” sur les services & observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'crm';
+  private subj = new Subject();
+  private behave = new BehaviorSubject(4);
+  private obs = new Observable((listXsubscribe) => {
+    listXsubscribe.next(Math.random());
+  });
+
+  constructor() {
+    // this.obs.subscribe((data) => console.log(data));
+    // this.obs.subscribe((data) => console.log(data));
+    // this.subj.next('test');
+    // this.subj.subscribe((data) => console.log(data));
+    // this.subj.next('test 2');
+    // this.subj.subscribe((data) => console.log(data));
+    // this.subj.next(Math.random());
+    // this.subj.subscribe((data) => console.log(data));
+    // this.subj.next('test 3')
+    // this.behave.subscribe((data) => console.log(data));
+    // this.behave.next(1);
+    // this.behave.next(2);
+    // this.behave.next(3);
+    // this.behave.subscribe((data) => console.log(data));
+    // this.behave.next(4);
+    // this.behave.next(Math.random());
+    // this.behave.subscribe((data) => console.log(data));
+    // this.behave.subscribe((data) => console.log(data));
+  }
 }
--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -8,7 +8,7 @@ import { OrdersService } from '../../services/orders.service';
 })
 export class PageListOrdersComponent implements OnInit {
   constructor(private ordersService: OrdersService) {
-    this.ordersService.collection$.subscribe((data)=> console.log(data));
+    this.ordersService.collection$.subscribe((data) => console.log(data));
     console.log('toto');
   }
 
